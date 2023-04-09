6fefe3722b07e9c037213c0ab44cf1a54bbaede2
José <30682875+jo-tm@users.noreply.github.com>
2023-04-09 18:26:43 +0200

    Arbitrary integers

--- sloth_permutation.c
+++ sloth_permutation.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 struct SlothPermutation {
-    int64_t p;
+    mpz_t p;
 };
 
 SlothPermutation* sloth_permutation_new(void) {
@@ -10,85 +10,80 @@ SlothPermutation* sloth_permutation_new(void) {
     if (!sp) {
         return NULL;
     }
-    sp->p = 170082004324204494273811327264862981553264701145937538369570764779791492622392118654022654452947093285873855529044371650895045691292912712699015605832276411308653107069798639938826015099738961427172366594187783204437869906954750443653318078358839409699824714551430573905637228307966826784684174483831608534979;
+    mpz_init_set_str(sp->p, "170082004324204494273811327264862981553264701145937538369570764779791492622392118654022654452947093285873855529044371650895045691292912712699015605832276411308653107069798639938826015099738961427172366594187783204437869906954750443653318078358839409699824714551430573905637228307966826784684174483831608534979", 10);
     return sp;
 }
 
 void sloth_permutation_free(SlothPermutation* sp) {
     if (sp) {
+        mpz_clear(sp->p);
         free(sp);
     }
 }
 
-bool sloth_permutation_sqrt_mod_p_verify(SlothPermutation* sp, int64_t y, int64_t x, int64_t p) {
-    int64_t temp = (y * y) % p;
-    if (temp != (x % p)) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-int64_t sloth_permutation_mod(SlothPermutation* sp, int64_t x, int64_t y) {
-    return (x - (x / y * y));
-}
-
-int64_t sloth_permutation_fast_pow(SlothPermutation* sp, int64_t base, int64_t exponent, int64_t modulus) {
-    if (modulus == 1) return 0;
-    int64_t result = 1;
-    base = base % modulus;
-    while (exponent > 0) {
-        if (exponent % 2 == 1) {
-            result = (result * base) % modulus;
-        }
-        exponent = exponent / 2;
-        base = (base * base) % modulus;
-    }
+bool sloth_permutation_sqrt_mod_p_verify(SlothPermutation* sp, mpz_t y, mpz_t x, mpz_t p) {
+    mpz_t temp;
+    mpz_init(temp);
+    mpz_powm_ui(temp, y, 2, p);
+    bool result = mpz_cmp(temp, x) == 0;
+    mpz_clear(temp);
     return result;
 }
 
-bool sloth_permutation_quad_res(SlothPermutation* sp, int64_t x) {
-    return sloth_permutation_fast_pow(sp, x, (sp->p - 1) / 2, sp->p) == 1;
+void sloth_permutation_mod(SlothPermutation* sp, mpz_t result, mpz_t x, mpz_t y) {
+    mpz_tdiv_r(result, x, y);
 }
 
-int64_t sloth_permutation_mod_sqrt_op(SlothPermutation* sp, int64_t x) {
-    int64_t y;
+void sloth_permutation_fast_pow(SlothPermutation* sp, mpz_t result, mpz_t base, mpz_t exponent, mpz_t modulus) {
+    mpz_powm(result, base, exponent, modulus);
+}
+
+bool sloth_permutation_quad_res(SlothPermutation* sp, mpz_t x) {
+    mpz_t temp;
+    mpz_init(temp);
+    mpz_sub_ui(temp, sp->p, 1);
+    mpz_tdiv_q_ui(temp, temp, 2);
+    sloth_permutation_fast_pow
+(sp, temp, x, temp, sp->p);
+    bool result = mpz_cmp_ui(temp, 1) == 0;
+    mpz_clear(temp);
+    return result;
+}
+
+void sloth_permutation_mod_sqrt_op(SlothPermutation* sp, mpz_t result, mpz_t x) {
+    mpz_t temp;
+    mpz_init(temp);
     if (sloth_permutation_quad_res(sp, x)) {
-        y = sloth_permutation_fast_pow(sp, x, (sp->p + 1) / 4, sp->p);
+        mpz_add_ui(temp, sp->p, 1);
+        mpz_tdiv_q_ui(temp, temp, 4);
+        sloth_permutation_fast_pow(sp, result, x, temp, sp->p);
     } else {
-        x = (sp->p - x) % sp->p;
-        y = sloth_permutation_fast_pow(sp, x, (sp->p + 1) / 4, sp->p);
+        mpz_sub(temp, sp->p, x);
+        mpz_mod(temp, temp, sp->p);
+        mpz_add_ui(temp, sp->p, 1);
+        mpz_tdiv_q_ui(temp, temp, 4);
+        sloth_permutation_fast_pow(sp, result, temp, temp, sp->p);
     }
-    return y;
+    mpz_clear(temp);
 }
 
-int64_t sloth_permutation_mod_op(SlothPermutation* sp, int64_t x, int64_t t) {
-    x = x % sp->p;
-    for (int64_t i = 0; i < t; i++) {
-        x = sloth_permutation_mod_sqrt_op(sp, x);
+void sloth_permutation_mod_op(SlothPermutation* sp, mpz_t result, mpz_t x, mpz_t t) {
+    mpz_mod(x, x, sp->p);
+    for (mpz_t i; mpz_cmp(i, t) < 0; mpz_add_ui(i, i, 1)) {
+        sloth_permutation_mod_sqrt_op(sp, x, x);
     }
-    return x;
+    mpz_set(result, x);
 }
 
-bool sloth_permutation_mod_verif(SlothPermutation* sp, int64_t y, int64_t x, int64_t t) {
-    x = x % sp->p;
-    for (int64_t i = 0; i < t; i++) {
-        y = (y * y) % sp->p;
+bool sloth_permutation_mod_verif(SlothPermutation* sp, mpz_t y, mpz_t x, mpz_t t) {
+    mpz_mod(x, x, sp->p);
+    for (mpz_t i; mpz_cmp(i, t) < 0; mpz_add_ui(i, i, 1)) {
+        mpz_powm_ui(y, y, 2, sp->p);
     }
     if (!sloth_permutation_quad_res(sp, y)) {
-        y = (sp->p - y) % sp->p;
+        mpz_sub(y, sp->p, y);
+        mpz_mod(y, y, sp->p);
     }
-    if ((x % sp->p) == y || ((sp->p - x) % sp->p) == y) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-int64_t sloth_permutation_generateProofVDF(SlothPermutation* sp, int64_t t, int64_t x) {
-    return sloth_permutation_mod_op(sp, x, t);
-}
-
-bool sloth_permutation_verifyProofVDF(SlothPermutation* sp, int64_t t, int64_t x, int64_t y) {
-    return sloth_permutation_mod_verif(sp, y, x, t);
+    bool result = mpz_cmp(x, y) == 0 || mpz_cmp(x, y) == mpz_cmp(sp->p, y);
+    return result;
 }
