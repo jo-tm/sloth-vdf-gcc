6542415febe8376618bcfad0dbdef2de66f3e02b
José <30682875+jo-tm@users.noreply.github.com>
2023-04-09 18:24:42 +0200

    Arbitray integers

--- sloth_permutation.h
+++ sloth_permutation.h
@@ -2,20 +2,19 @@
 #define SLOTH_PERMUTATION_H
 
 #include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
+#include <gmp.h>
 
 typedef struct SlothPermutation SlothPermutation;
 
 SlothPermutation* sloth_permutation_new(void);
 void sloth_permutation_free(SlothPermutation* sp);
 
-bool sloth_permutation_sqrt_mod_p_verify(SlothPermutation* sp, int64_t y, int64_t x, int64_t p);
-int64_t sloth_permutation_mod(SlothPermutation* sp, int64_t x, int64_t y);
-int64_t sloth_permutation_fast_pow(SlothPermutation* sp, int64_t base, int64_t exponent, int64_t modulus);
-bool sloth_permutation_quad_res(SlothPermutation* sp, int64_t x);
-int64_t sloth_permutation_mod_sqrt_op(SlothPermutation* sp, int64_t x);
-int64_t sloth_permutation_mod_op(SlothPermutation* sp, int64_t x, int64_t t);
-bool sloth_permutation_mod_verif(SlothPermutation* sp, int64_t y, int64_t x, int64_t t);
+bool sloth_permutation_sqrt_mod_p_verify(SlothPermutation* sp, mpz_t y, mpz_t x, mpz_t p);
+void sloth_permutation_mod(SlothPermutation* sp, mpz_t result, mpz_t x, mpz_t y);
+void sloth_permutation_fast_pow(SlothPermutation* sp, mpz_t result, mpz_t base, mpz_t exponent, mpz_t modulus);
+bool sloth_permutation_quad_res(SlothPermutation* sp, mpz_t x);
+void sloth_permutation_mod_sqrt_op(SlothPermutation* sp, mpz_t result, mpz_t x);
+void sloth_permutation_mod_op(SlothPermutation* sp, mpz_t result, mpz_t x, mpz_t t);
+bool sloth_permutation_mod_verif(SlothPermutation* sp, mpz_t y, mpz_t x, mpz_t t);
 
 #endif // SLOTH_PERMUTATION_H
