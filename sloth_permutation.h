b17da77d9e4a4cfd8aa38d6cffaeed47e9212ae3
José <30682875+jo-tm@users.noreply.github.com>
2023-04-10 00:58:51 +0200

    Update sloth_permutation.h

--- sloth_permutation.h
+++ sloth_permutation.h
@@ -15,7 +15,8 @@ void sloth_permutation_mod(SlothPermutation* sp, mpz_t result, mpz_t x, mpz_t y)
 void sloth_permutation_fast_pow(SlothPermutation* sp, mpz_t result, mpz_t base, mpz_t exponent, mpz_t modulus);
 bool sloth_permutation_quad_res(SlothPermutation* sp, mpz_t x);
 void sloth_permutation_mod_sqrt_op(SlothPermutation* sp, mpz_t result, mpz_t x);
-void sloth_permutation_mod_op(SlothPermutation* sp, mpz_t result, mpz_t x, mpz_t t);
+
+void sloth_generate_proof_vdf(SlothPermutation* sp, mpz_t result, mpz_t x, mpz_t t);
 bool sloth_permutation_mod_verif(SlothPermutation* sp, mpz_t y, mpz_t x, mpz_t t);
 
 #endif // SLOTH_PERMUTATION_H
