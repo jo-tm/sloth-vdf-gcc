1282951f223991b79ef7d57c73c3aa54020cefec
José <30682875+jo-tm@users.noreply.github.com>
2023-04-10 02:02:50 +0200

    Update sloth_permutation.h

--- sloth_permutation.h
+++ sloth_permutation.h
@@ -26,5 +26,6 @@ bool sloth_verify_proof_vdf(SlothPermutation* sp, mpz_t y, mpz_t x, mpz_t t);
 SlothPermutation* read_biguint64_le(uint8_t* buffer, int offset);
 void write_biguint64_le(SlothPermutation *sp, mpz_t x, uint8_t *buffer, size_t offset);
 SlothPermutation* read_biguint_le(const uint8_t* buffer, size_t byte_len, size_t offset);
+void write_biguint_le(SlothPermutation* sp, mpz_t x, uint8_t* buffer, int byte_len, int offset); 
 
 #endif // SLOTH_PERMUTATION_H
